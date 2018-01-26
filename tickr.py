a38ed3ccfa16a411fb5fb08288da94bf8bbbb34b
kelr <kruan6818@gmail.com>
2018-01-25 23:13:05 -0500

    Got the thread to do the things

--- tickr.py
+++ tickr.py
@@ -1,22 +1,9 @@
 import gdax
 import time
-from Tkinter import *
+from tkinter import *
 from threading import Thread
+import time
 
-
-class View(Frame):
-   
-   def __init__(self, master=None):
-        Frame.__init__(self, master)               
-        self.master = master
-        self.data = Data()
-
-        l = Label(self.master, textvariable = gdax)
-        l.pack()
-
-root = Tk()
-app = View(root)
-root.mainloop()
 
 class Data():
 
@@ -24,4 +11,36 @@ class Data():
         self.public_client = gdax.PublicClient()
 
     def get_tick(self):
+        print("hi")
         return self.public_client.get_product_ticker(product_id="ETH-USD")["price"]
+
+class View(Frame):
+   
+    def __init__(self, master=None):
+        Frame.__init__(self, master)               
+        self.master = master
+        self.create_window()
+
+
+    def create_window(self):
+        self.data = Data()
+        self.price = StringVar()
+
+        display = Message(self.master, textvariable=self.price)
+        display.grid(row=2, column=1)
+        display.pack()
+
+        self.update_thread = Thread(target=self.update_price)
+        self.update_thread.start()
+
+    def update_price(self):
+        while(1):
+            self.price.set(self.data.get_tick())
+            time.sleep(0.1)
+
+
+root = Tk()
+app = View(root)
+root.mainloop()
+
+
