91f6136d09282c64049359fd815044297732741c
kelr <kruan6818@gmail.com>
2018-01-25 23:30:18 -0500

    Basically a 30 min project not even

--- tickr.py
+++ tickr.py
@@ -23,18 +23,32 @@ class View(Frame):
 
     def create_window(self):
         self.data = Data()
-        self.price = IntVar()
-        self.current = IntVar()
+        self.price = DoubleVar()
+        self.current = DoubleVar()
+        self.final = DoubleVar()
 
-        display = Message(self.master, textvariable=self.price)
-        display.grid(row=2, column=1)
+        eth = Label(self.master, text="ETH-USD Last Price:")
+        eth.pack()
+        lel2 = Label(self.master, text="")
+        lel2.pack()
+
+        display = Label(self.master, textvariable=self.price)
         display.pack()
 
-        display_3 = Message(self.master, textvariable=self.current*self.price)
+        lel = Label(self.master, text="Held Volume:")
+        lel.pack()
+
+        lel3 = Label(self.master, text="")
+        lel3.pack()
+
+        display_3 = Label(self.master, textvariable=self.final)
         display_3.pack()
 
         display2 = Entry(self.master, textvariable=self.current)       
         display2.pack()
+
+        button = Button(self.master, text="Confirm", command=self.update_volume)
+        button.pack()
 
         self.update_thread = Thread(target=self.update_price)
         self.update_thread.start()
@@ -44,8 +58,13 @@ class View(Frame):
             self.price.set(self.data.get_tick())
             time.sleep(0.1)
 
+    def update_volume(self):
+        self.final.set(self.price.get() * self.current.get())
+
+
 
 root = Tk()
+root.geometry("200x200")
 app = View(root)
 root.mainloop()
 
